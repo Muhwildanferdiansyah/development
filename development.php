e070c640781ca6b9fd31356021ee04e1f7af17de
Muhwildanferdiansyah <73091498+Muhwildanferdiansyah@users.noreply.github.com>
2021-11-30 17:21:49 +0700

    update deskripsi

--- development.php
+++ development.php
@@ -15,8 +15,8 @@
  * @wordpress-plugin
  * Plugin Name:       development
  * Plugin URI:        https://www.yukdiorder.com/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.1
+ * Description:       Catat pekerjaan harian 
+ * Version:           1.0.2
  * Author:            yukdiorder
  * Author URI:        https://www.yukdiorder.com/
  * License:           GPL-2.0+
